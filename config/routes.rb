67562002140f2e87d144fb21a70f46565ad5d4ab
pascales <pascales.dedy@gmail.com>
2018-05-26 11:40:50 +0700

    [pascales] adding api versioning v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :paintings
+	namespace :v1 do
+		resources :paintings
+	end
+
 end
