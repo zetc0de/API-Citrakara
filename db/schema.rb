5c43687794a87a14ebc1c35691b43900605def2e
pascales <pascales.dedy@gmail.com>
2018-06-27 05:28:14 +0700

    [pascales] adding username on painting index response

--- db/schema.rb
+++ db/schema.rb
@@ -40,14 +40,6 @@ ActiveRecord::Schema.define(version: 2018_06_26_174529) do
     t.index ["user_id"], name: "index_feedbacks_on_user_id"
   end
 
-  create_table "genres", force: :cascade do |t|
-    t.string "genretitle"
-    t.bigint "painting_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["painting_id"], name: "index_genres_on_painting_id"
-  end
-
   create_table "paintings", force: :cascade do |t|
     t.string "title"
     t.string "description"
